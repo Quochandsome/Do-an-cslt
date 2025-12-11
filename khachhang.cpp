45f710a90d432923f128b0bb4089ed4e0ff22a18
quantapcot <quantruong20827@gmail.com>
2025-12-11 07:49:28 +0000

    lấy thông tin khách hàng

--- khachhang.cpp
+++ khachhang.cpp
@@ -1,1 +1,11 @@
-#include <iosteam>
+#include <iostream>
+#include <string>
+#include <format>
+using std::cout, std::cin, std::string, std::format;
+struct Customer {
+    string id;
+    string name;
+    string phoneNum;
+    string personalId;
+    string address;
+}
