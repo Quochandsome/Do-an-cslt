df72f7c7fe1e7cdb795463a65da471b90958e5db
quantapcot <quantruong20827@gmail.com>
2025-12-12 23:15:37 +0700

    Quản lý khách hàng
    Added gender field to Customer structure and updated related functions to handle gender input and output.

--- khachhang.cpp
+++ khachhang.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using std::cout, std::cin, std::string;
+#include <iomanip>
+
+using std::cout, std::cin, std::string, std::getline, std::ifstream, std::ofstream, std::setw, std::left, std::right;
 
 struct Customer {
     string id;
@@ -9,6 +11,7 @@ struct Customer {
     string phoneNum;
     string personalId;
     string address;
+    string gender;
 };
 
 const int MAX_CUSTOMERS = 100;
@@ -24,6 +27,7 @@ void readFile() {
         getline(fileIn, cusList[customerQuantity].phoneNum);
         getline(fileIn, cusList[customerQuantity].personalId);
         getline(fileIn, cusList[customerQuantity].address);
+        getline(fileIn, cusList[customerQuantity].gender);
         customerQuantity++;
     }
     fileIn.close();
@@ -31,141 +35,106 @@ void readFile() {
 
 void writeFile() {
     ofstream fileOut("data_khach.txt");
-    for(int i = 0; i <= customerQuantity - 1; ++i) {
-        fileOut << cusList[i].id << endl;
-        fileOut << cusList[i].name << endl;
-        fileOut << cusList[i].phoneNum << endl;
-        fileOut << cusList[i].personalId << endl;
-        fileOut << cusList[i].address << endl;
-
+    for(int i = 0; i < customerQuantity; ++i) {
+        fileOut << cusList[i].id << "\n"
+                << cusList[i].name << "\n"
+                << cusList[i].phoneNum << "\n"
+                << cusList[i].personalId << "\n"
+                << cusList[i].address << "\n"
+                << cusList[i].gender << "\n";
     }
     fileOut.close();
 }
 
-// hàm thêm khách hàng
 void addCustomer() {
     Customer cus;
-    cout << "Nhập ID : "; 
-    getline(cin, cus.id);
-    cout << "Nhập Họ và tên: ";
-    getline(cin, cus.name);
-    cout << "Nhập SĐT: ";
-    getline(cin, cus.phoneNum);
-    cout << "Nhập mã CCCD: ";
-    getline(cin, cus.personalId);
-    cout << "Nhập địa chỉ: ";
-    getline(cin, cus.address);
+    cout << "Nhập ID : "; getline(cin, cus.id);
+    cout << "Nhập Họ và tên: "; getline(cin, cus.name);
+    cout << "Nhập SĐT: "; getline(cin, cus.phoneNum);
+    cout << "Nhập mã CCCD: "; getline(cin, cus.personalId);
+    cout << "Nhập địa chỉ: "; getline(cin, cus.address);
+    cout << "Nhập giới tính: "; getline(cin, cus.gender);
     cusList[customerQuantity++] = cus;
     writeFile();
-    cout << "Thêm khách hàng thành công!\n";
+    cout << "\n>>> Thêm khách hàng thành công!\n";
 }
 
-// hàm xem danh sách khách hàng
 void showList() {
-    for(int i = 0; i <= customerQuantity - 1; ++i) {
-        cout << "ID Khách hàng: " << cusList[i].id << endl;
-        cout << "Họ và tên: " << cusList[i].name << endl;
-        cout << "SĐT: " << cusList[i].phoneNum << endl;
-        cout << "Mã CCCD: " << cusList[i].personalId << endl;
-        cout << "Địa chỉ: " << cusList[i].address << endl;
-        cout << "--------------------------\n";
-
+    cout << "\n--- DANH SÁCH KHÁCH HÀNG ---\n";
+    cout << "|ID       |Họ Tên             |SĐT            |CCCD           |Địa Chỉ           |Giới Tính|\n";
+    cout << "---------------------------------------------------------------------------------------------\n";
+    for(int i = 0; i < customerQuantity; ++i) {
+        cout << "|"
+             << setw(9)  << left << cusList[i].id << "|"
+             << setw(19) << left << cusList[i].name << "|"
+             << setw(15) << left << cusList[i].phoneNum << "|"
+             << setw(15) << left << cusList[i].personalId << "|"
+             << setw(18) << left << cusList[i].address << "|"
+             << setw(10) << left << cusList[i].gender << "|\n";
     }
+    cout << "---------------------------------------------------------------------------------------------\n";
 }
 
-// hàm tìm khách hàng
 void findCustomer(string id) {
-    for (int i = 0; i <= customerQuantity - 1; ++i) {
+    for (int i = 0; i < customerQuantity; ++i) {
         if (cusList[i].id == id) {
-            cout << "Khách hàng bạn muốn tìm là:\n";
-            cout << "Họ và tên: " << cusList[i].name << endl;
-            cout << "SĐT: " << cusList[i].phoneNum << endl;
-            cout << "Mã CCCD: " << cusList[i].personalId << endl;
-            cout << "Địa chỉ: " << cusList[i].address << endl;
+            cout << "\n--- THÔNG TIN KHÁCH HÀNG ---\n";
+            cout << "|ID       |Họ Tên             |SĐT            |CCCD           |Địa Chỉ           |Giới Tính|\n";
+            cout << "---------------------------------------------------------------------------------------------\n";
+            cout << "|"
+                 << setw(9)  << left << cusList[i].id << "|"
+                 << setw(19) << left << cusList[i].name << "|"
+                 << setw(15) << left << cusList[i].phoneNum << "|"
+                 << setw(15) << left << cusList[i].personalId << "|"
+                 << setw(18) << left << cusList[i].address << "|"
+                 << setw(10) << left << cusList[i].gender << "|\n";
+            cout << "---------------------------------------------------------------------------------------------\n";
             return;
         }
     }
+    cout << "\n>>> Không tìm thấy khách hàng với ID này!\n";
 }
 
-// hàm sửa thông tin khách hàng
 void fixCustomer(string id) {
     for (int i = 0; i < customerQuantity; ++i) {
         if (cusList[i].id == id) {
-            cout << "Nhập thông tin mới:\n";
-            cout << "Nhập Họ và tên: "; getline(cin, cusList[i].name);
-            cout << "Nhập SĐT: "; getline(cin, cusList[i].phoneNum);
-            cout << "Nhập Mã CCCD: "; getline(cin, cusList[i].personalId);
-            cout << "Nhập Địa chỉ: "; getline(cin, cusList[i].address);
+            cout << "\n>>> Nhập thông tin mới cho khách hàng:\n";
+            cout << "Họ và tên: "; getline(cin, cusList[i].name);
+            cout << "SĐT: "; getline(cin, cusList[i].phoneNum);
+            cout << "CCCD: "; getline(cin, cusList[i].personalId);
+            cout << "Địa chỉ: "; getline(cin, cusList[i].address);
+            cout << "Giới tính: "; getline(cin, cusList[i].gender);
             writeFile();
-            cout << "Sửa thông tin thành công!\n";
+            cout << "\n>>> Sửa thông tin thành công!\n";
             return;
         }
     }
+    cout << "\n>>> Không tìm thấy khách hàng để sửa!\n";
 }
 
-// hàm xóa thong tin khách hàng
 void deleteCustomer(string id) {
-    for (int i = 0; i <= customerQuantity - 1; ++i) {
+    for (int i = 0; i < customerQuantity; ++i) {
         if (cusList[i].id == id) {
             for (int j = i; j < customerQuantity - 1; j++) {
                 cusList[j] = cusList[j + 1];
             }
             --customerQuantity;
             writeFile();
-            cout << "Xóa khách hàng thành công!\n";
+            cout << "\n>>> Xóa khách hàng thành công!\n";
             return;
         }
     }
+    cout << "\n>>> Không tìm thấy khách hàng để xóa!\n";
 }
 
 int main() {
     string buffer;
     int choose;
     do {
-        cout << "\n===== MENU =====\n";
-        cout << "1. Thêm khách hàng\n";
-        cout << "2. Xem danh sách\n";
-        cout << "3. Tìm khách hàng\n";
-        cout << "4. Sửa khách hàng\n";
-        cout << "5. Xóa khách hàng\n";
-        cout << "0. Thoát\n";
-        cout << "Chọn số theo nhu cầu của bạn "; 
-        getline(cin, buffer);
-        choose = stoi(buffer);
-
-        switch (choose) {
-            case 1: {
-                addCustomer();
-                break;
-            } 
-            case 2: {
-                showList(); 
-                break;
-            }
-            case 3: {
-                int id; 
-                cout << "Hãy nhập ID khách hàng cần tìm: "; cin >> id;
-                findCustomer(id);
-                break;
-            }
-            case 4: {
-                int id; 
-                cout << "Hãy nhập ID khách hàng cần sửa: "; cin >> id;
-                fixCustomer(id);
-                break;
-            }
-            case 5: {
-                int id; 
-                cout << "Nhap ID can xoa: "; cin >> id;
-                deleteCustomer(id);
-                break;
-            }
-            case 0: {
-                cout << "Thoát chương trình.\n"; 
-                break;
-            }
-        }
-    } while (choose != 0);
-
-    return 0;
-}
+        cout << "\n========================================\n";
+        cout << "        QUẢN LÝ KHÁCH HÀNG\n";
+        cout << "========================================\n";
+        cout << " [1] Thêm khách hàng\n";
+        cout << " [2] Xem danh sách khách hàng\n";
+        cout << " [3] Tìm khách hàng theo ID\n";
+        cout << " [4] Sửa thông tin khách hàng\n";
